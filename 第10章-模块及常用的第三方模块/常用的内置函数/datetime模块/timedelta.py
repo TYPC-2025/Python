2b3fb290e9a8d9030738c96a9dfb188309ba745b
TYPC-2025 <tangyaowinty@outlook.com>
2025-08-11 22:18:11 +0800

    timedelta

---
+++ 第10章-模块及常用的第三方模块/常用的内置函数/datetime模块/timedelta.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+from datetime import timedelta
+#创建两个datetime类型的对象
+deltal=datetime(2025,10,1)-datetime(2025,5,1)
+print('deltal的数据类型是:',type(deltal),'deltal表示的数据是:',deltal)
+print('2025年5月1日之后的')
